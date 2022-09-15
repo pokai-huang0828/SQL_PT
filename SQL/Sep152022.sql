bc72afd77a73a9d3623e22107bda940164b47de2
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-15 21:22:06 +0800

    Added new note3

--- SQL/Sep152022.sql
+++ SQL/Sep152022.sql
@@ -139,4 +139,128 @@ WHILE @id<=@maxId
 
 ---------------------------------------------------------------
 
+---- CASE
+SELECT 員工編號, 姓名, 職稱, 稱呼 FROM 員工
+
+
+SELECT 員工編號, 姓名, 職稱, '母' AS 性別 FROM 員工 WHERE 稱呼='小姐'
+UNION ALL 
+SELECT 員工編號, 姓名, 職稱, '公' FROM 員工 WHERE 稱呼='先生'
+
+
+SELECT 員工編號, 姓名, 職稱, 
+	CASE 稱呼
+		WHEN '小姐' THEN '母'
+		WHEN '先生' THEN '公'
+	END AS 性別
+FROM 員工
+
+SELECT 產品編號, 產品, 單價, 
+	CASE 
+		WHEN 單價>=100 THEN '高價位'
+		WHEN 單價>=50 THEN '中價位'
+		WHEN 單價>=20 THEN '低價位'
+		ELSE '超便宜' END AS 價位等級
+FROM 產品資料
+
+-------------------------------------------------------------------------------
+
+/*
+CASE 
+	WHEN 1 THEN INSERT INTO ....
+	WHEN 2 THEN UPDATE......
+	WHEN 3 THEN DELETE FROM ......
+	ELSE SELECT * FROM .....
+END
+*/
+
+--IIF(邏輯判斷, 成立值, 不成立值)
+SELECT 員工編號,姓名,職稱,IIF(稱呼='小姐','母','公') AS 性別
+FROM 員工
+
+SELECT 產品編號,產品,單價
+	,IIF(單價>=100,'高價位',IIF(單價>=50,'中價位',IIF(單價>=20,'低價位','超便宜'))) AS 價位等級
+FROM 產品資料
+
+---------------------------------------------------------------------------------------------------
+
+--CHOOSE(選項, 值1, 值2, 值3, ... )
+
+SELECT CHOOSE(2,'AA','BB','CC','DD','EE')
+SELECT CHOOSE(4,'AA','BB','CC','DD','EE')
+
+
+SELECT 訂單號碼,客戶編號,訂單日期,送貨方式 FROM 訂貨主檔;	--0.0138
+
+SELECT 訂單號碼,客戶編號,訂單日期,B.貨運公司名稱
+FROM 訂貨主檔 AS A JOIN 貨運公司 AS B ON A.送貨方式=B.貨運公司編號;		--0.0418
+
+SELECT 訂單號碼,客戶編號,訂單日期
+	,CHOOSE(送貨方式,'快遞','郵寄','親送') AS 運送方式
+FROM 訂貨主檔;	--0.0139
+
+-------------------------------------------------------------------------------------------------------
+
+--- RAISERROR( 錯誤號 OR 錯誤訊息， 錯誤等級1-25， 狀態1-127) 
+
+RAISERROR('這是自訂的錯誤訊息', 16, 10);
+
+UPDATE 產品資料 SET 單價=500 WHERE 產品編號=500;
+IF @@ROWCOUNT = 0 RAISERROR('未更新到任何資料!!', 16, 10);
+
+
+--- THROW 錯誤號(50000以上), 錯誤訊息, 狀態1-127   一律錯誤等級16
+THROW 50010, '這是自訂的錯誤訊息!!', 10;
+
+---------------------------------------------------------------------------
+
+EXECUTE sp_helpdb;
+EXEC sp_helpdb;
+
+--- 可執行 文字型態的SQL 命令
+EXECUTE ('SELECT * FROM 產品資料');
+
+DECLARE @NAME1 NVARCHAR(10);
+SET @NAME1='產品資料';
+SELECT * FROM @NAME1;
+
+
+/*
+SELECT * FROM '產品資料';
+SELECT * FROM 產品資料;
+*/
+
+DECLARE @sql NVARCHAR(100);
+SET @sql='SELECT * FROM ';
+DECLARE @name NVARCHAR(10);
+SET @name='產品資料';
+
+--PRINT @sql+@name;
+EXECUTE(@sql+@name);
+
+
+DECLARE @sql NVARCHAR(100);
+SET @sql='SELECT * FROM ';
+DECLARE @name NVARCHAR(10);
+SET @name='產品資料';
+EXECUTE(@sql+@name);
+
+SET @name='產品類別';
+EXECUTE(@sql+@name);
+
+SET @name='客戶';
+EXECUTE(@sql+@name);
+
+---------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
 
