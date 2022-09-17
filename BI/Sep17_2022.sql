229025abd444459335997b8b56e62b36c94d2080
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-17 09:49:16 +0800

    Added new class note

---
+++ BI/Sep17_2022.sql
@@ -0,0 +1,17 @@
+--SELECT COUNT(*) FROM sys.tables WHERE [name]='北風產品'
+
+IF (SELECT COUNT(*) FROM sys.tables WHERE [name]='北風產品')>0
+	TRUNCATE TABLE [北風產品];
+ELSE
+  BEGIN
+	CREATE TABLE 北風產品
+	(
+	   產品編號 INT,
+	   產品名稱 NVARCHAR(20),
+	   供應商編號 INT,
+	   類別編號 INT,
+	   單價 MONEY,
+	   單位數量 NVARCHAR(5),
+	   庫存量 INT
+	)
+  END
