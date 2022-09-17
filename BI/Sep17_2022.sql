9c5206df5f0795946795032efd8c49a88d860082
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-17 16:30:39 +0800

    Added new class note2

--- BI/Sep17_2022.sql
+++ BI/Sep17_2022.sql
@@ -1,4 +1,13 @@
---SELECT COUNT(*) FROM sys.tables WHERE [name]='北風產品'
+
+----  課程教學進度
+--- 成功/失敗通知
+--- 時序容器
+
+--- 控制台 -> 服務 -> DTC (Distributed Transaction Coordinator) Check Enable!
+--- 分散式交易協調器
+
+--- 錯誤報告 OLE DB 目的地錯誤輸出 
+--- 利用時序容器調整偵錯順序
 
 IF (SELECT COUNT(*) FROM sys.tables WHERE [name]='北風產品')>0
 	TRUNCATE TABLE [北風產品];
@@ -21,6 +30,8 @@ ELSE
 DROP TABLE 北風產品
 SELECT * FROM 北風產品
 
+--- 顯示->其他視窗-> variable
 
---- 控制台 -> 服務 -> DTC (Distributed Transaction Coordinator) Check Enable!
---- 分散式交易協調器
+---  1. 每次匯出 北風客戶_20220917_162430.csv
+---  2. 每次備分資料庫
+---  提示 : 執行 SQL 工作編輯器 -> SQLSourseType -> 變數
