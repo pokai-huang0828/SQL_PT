b54432a5fdaf50605652d1a00ce92ddc5c9f7b88
pokai-huang0828 <kenny0963676060@gmail.com>
2022-09-24 12:12:50 +0800

    Added new class note2

--- BI/Sep24_2022.sql
+++ BI/Sep24_2022.sql
@@ -14,3 +14,22 @@ GO
 
 SELECT * FROM [北風產品2] order by 產品編號
 DROP TABLE [北風產品2]
+
+------------------------------------------------------
+
+SELECT 客戶編號, 公司名稱, 連絡人, 連絡人職稱, 電話, 'kenny0963676060@gmail.com' AS 電子郵件
+INTO 練習.dbo.EDM
+FROM 中文北風.dbo.客戶
+WHERE 地址 LIKE '桃園%'
+
+
+--信件底稿
+/*
+@[User::CID] +"  "+ @[User::CCompanyName] +"\n"+
+"親愛的 "+ @[User::CPerson] +" "+ @[User::CPersonJob]  +" 您好：\n"+
+"本公司跳樓大拍賣！\n"+
+"請快來搶購！\n\n\n巨匠公司 敬上"
+*/
+
+
+
