f63573f8418c35f43d1d60805d3c893f24cf0cab
pokai-huang0828 <kenny0963676060@gmail.com>
2022-10-04 20:16:14 +0800

    Added class note1

--- SQL/Oct042022.sql
+++ SQL/Oct042022.sql
@@ -102,3 +102,92 @@ SELECT * FROM 匠匠;
 SELECT * FROM 匠匠追蹤表;
 
 --------------------------------------------------------
+
+--SQL2016 時態表
+
+--DROP TABLE 新巨巨;
+--SELECT * FROM sys.tables
+
+
+CREATE TABLE dbo.新巨巨
+(    
+	產品編號 INT NOT NULL PRIMARY KEY
+	, 產品名稱 NVARCHAR(10) NOT NULL  
+	, 售價 MONEY NOT NULL     
+	, [開始時間] DATETIME2(2) GENERATED ALWAYS AS ROW START  
+	, [結束時間] DATETIME2(2) GENERATED ALWAYS AS ROW END  
+	, PERIOD FOR SYSTEM_TIME (開始時間, 結束時間)
+)    
+WITH (SYSTEM_VERSIONING = ON(HISTORY_TABLE = dbo.新巨巨追蹤));  
+
+
+DROP TABLE 新巨巨;
+ALTER TABLE 新巨巨 SET (SYSTEM_VERSIONING = OFF);
+DROP TABLE 新巨巨;
+DROP TABLE [dbo].[MSSQL_TemporalHistoryFor_1525580473];
+
+INSERT INTO 新巨巨(產品編號, 產品名稱, 售價)
+	SELECT 產品編號, 品名, 價錢 FROM 練練.dbo.巨巨;
+
+SELECT * FROM 新巨巨;
+
+INSERT INTO 新巨巨(產品編號,產品名稱,售價) VALUES(30,'波卡',35);
+DELETE FROM 新巨巨 WHERE 產品編號=29;
+UPDATE 新巨巨 SET 產品名稱='大金沙',售價=180 WHERE 產品編號=28;
+INSERT INTO 新巨巨(產品編號,產品名稱,售價) VALUES(31,'芝多司',30);
+UPDATE 新巨巨 SET 產品名稱='大波卡',售價=50 WHERE 產品編號=30;
+UPDATE 新巨巨 SET 產品名稱='大盒金沙',售價=280 WHERE 產品編號=28;
+
+
+SELECT * FROM 新巨巨;
+SELECT * FROM 新巨巨 FOR SYSTEM_TIME ALL;
+SELECT * FROM 新巨巨 FOR SYSTEM_TIME ALL WHERE 產品編號= 28 ORDER BY 結束時間;
+
+ALTER TABLE 新巨巨 SET (SYSTEM_VERSIONING = OFF);
+ALTER TABLE 新巨巨 SET (SYSTEM_VERSIONING = ON(HISTORY_TABLE = dbo.新巨巨追蹤));
+
+-----------------------------------------------------
+
+/*--- ACID 交易行為(NON-SQL 沒有)
+ACID，是指資料庫管理系統（DBMS）在寫入或更新資料的過程中，
+為保證事務（transaction）是正確可靠的，所必須具備的四個特性：
+	1. 原子性（atomicity，或稱不可分割性）
+	2. 一致性（consistency）
+	3. 隔離性（isolation，又稱獨立性）
+	4. 持久性（durability）
+
+在資料庫系統中，一個事務是指：由一系列資料庫操作組成的一個完整的邏輯過程。
+	例如銀行轉帳，從原帳戶扣除金額，以及向目標帳戶添加金額，這兩個資料庫操作的總和，構成一個完整的邏輯過程，不可拆分。
+	這個過程被稱為一個事務，具有ACID特性。
+*/
+
+
+SELECT * INTO 新匠匠 FROM 練練.dbo.匠匠;
+
+
+SELECT * FROM 新匠匠;
+
+DELETE FROM 新匠匠 WHERE 產品編號>=26;
+
+INSERT INTO 新匠匠 VALUES(26,'AAA',260);
+INSERT INTO 新匠匠 VALUES(27,'BBB','BBB');
+INSERT INTO 新匠匠 VALUES(28,'CCC',280);
+---------------
+INSERT INTO 新匠匠 VALUES(26,'AAA',260),(27,'BBB','BBB'),(28,'CCC',280);
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT * FROM 新匠匠;
+
+--BEGIN TRANSACTION   搭配 COMMIT / ROLLBACK
+BEGIN TRAN
+	INSERT INTO 新匠匠 VALUES(26,'AAA',260);
+	INSERT INTO 新匠匠 VALUES(27,'BBB','BBB');
+	INSERT INTO 新匠匠 VALUES(28,'CCC',280);
+COMMIT
+
+BEGIN TRAN
+	INSERT INTO 新匠匠 VALUES(26,'AAA',260);
+	INSERT INTO 新匠匠 VALUES(27,'BBB',270);
+	INSERT INTO 新匠匠 VALUES(28,'CCC',280);
+ROLLBACK
